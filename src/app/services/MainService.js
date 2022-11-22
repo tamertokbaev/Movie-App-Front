050b38b5819b376dc2150b8858ddfe1a9ec34131
tamertokbaev <tokbaev201536@gmail.com>
2022-11-22 09:02:09 +0600

    movie detail page

--- src/app/services/MainService.js
+++ src/app/services/MainService.js
@@ -1,15 +1,24 @@
 import http from "../utils/http";
 const BASE_URL = `http://127.0.0.1:8000/api/movie`
 
-export const getFeaturedMovies = () => {
+const getFeaturedMovies = () => {
   return http.get(`${BASE_URL}/featured`)
 }
 
-export const getPopularMovies = () => {
+const getPopularMovies = () => {
   return http.get(`${BASE_URL}/popular`)
+}
+
+const getMovieInfo = (movieId) => {
+  return http.get(`${BASE_URL}/info`, {
+    params: {
+      movie_id: movieId
+    }
+  })
 }
 
 export const MainService = {
   getFeaturedMovies,
-  getPopularMovies
+  getPopularMovies,
+  getMovieInfo
 }
