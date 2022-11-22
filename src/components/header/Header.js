90bc137aaa04381b1c743c09d61127de158c5150
tamertokbaev <tokbaev201536@gmail.com>
2022-11-22 12:41:02 +0600

    Menu in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,15 +3,19 @@ import s from "./Header.module.scss"
 import {Link} from "react-router-dom"
 import clsx from "clsx";
 import {useUserContext} from "../../app/context/userContext";
-import {Avatar, Button, IconButton, Tooltip} from "@mui/material";
+import {Avatar, Button, IconButton, Menu, MenuItem, Tooltip} from "@mui/material";
 
 const Header = ({fluid}) => {
   const {userInfo, updateUserInfo} = useUserContext()
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
-  const handleClick = () => {
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
 
+  const logOut = () => {
+    localStorage.removeItem('auth_token')
   }
 
   return (
@@ -46,6 +50,49 @@ const Header = ({fluid}) => {
           )
         }
       </div>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        onClose={() => setAnchorEl(null)}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem disabled onClick={() => setAnchorEl(null)}>Профиль</MenuItem>
+        <MenuItem onClick={() => setAnchorEl(null)}>
+          <Link style={{textDecoration: "none", color: "inherit"}} to="/admin/movie/add">
+            Админ панель
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={logOut}>Выйти</MenuItem>
+      </Menu>
     </div>
   )
 }
